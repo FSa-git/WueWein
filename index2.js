f2ef47ab9a8c5895d946ecd718f4b07555de0b5e
FS <francesco9721@gmail.com>
2023-05-20 23:52:54 +0200

    changed variables 'var' to 'let'

--- index2.js
+++ index2.js
@@ -3,11 +3,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 // Popup-Inhalt und Styling definieren
-var popupContent = "<h1>Altersüberprüfung</h1><p>Um diese Seite zu besuchen, musst du älter als 18 Jahre alt sein.</p><button id='older'>Ja, ich bin 18</button><button id='younger' style='margin-left: 50px'>Nein, ich bin jünger</button>";
-var popupStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: black; padding: 700px; width: 100%; height: 250px; text-align: center;";
+let popupContent = "<h1>Altersüberprüfung</h1><p>Um diese Seite zu besuchen, musst du älter als 18 Jahre alt sein.</p><br><button id='older' style='padding: 20px'>Ja, ich bin 18</button><button id='younger' style='margin-left: 50px; padding: 20px'>Nein, ich bin jünger</button>";
+let popupStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: black; padding: 700px; width: 100%; height: 250px; text-align: center;";
 
 // Popup-Element erstellen und Inhalt einfügen
-var popup = document.createElement("div");
+let popup = document.createElement("div");
 popup.innerHTML = popupContent;
 popup.style = popupStyle;
 
@@ -21,6 +21,18 @@ document.getElementById("older").addEventListener("click", function () {
 
 // Anderer Knopf wird gedrückt 
 document.getElementById('younger').addEventListener('click',()=>{
-    window.location.href = 'alter.html';
+    
+    // Lässt auf eine andere Seite weiterleiten
+    // window.location.href = 'alter.html';
+
+    let newContent = "<h1>Schade..</h1><p>Um diese Seite zu besuchen, musst du älter als 18 Jahre alt sein.</p>";
+    let newContentStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: black; padding: 700px; width: 100%; height: 250px; text-align: center;";
+
+    let newWindow = document.createElement("div");
+    newWindow.innerHTML = newContent;
+    newWindow.style = newContentStyle;
+
+    document.body.appendChild(newWindow);
+
 });
 });
