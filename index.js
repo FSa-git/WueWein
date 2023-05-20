b8508ef44b46c5aa53f97239dbb1361d037ba952
FS <francesco9721@gmail.com>
2023-05-20 23:25:26 +0200

    Richtige Altersüberprüfung geschafft

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
 
 // });
 
+
+
 document.addEventListener('click',(e)=>{
 
 let over = document.getElementById('age_over');
@@ -18,7 +20,17 @@ let under = document.getElementById('age_under');
         window.location.href = 'index.html';
     }
     else if(e.target === under){
-        window.location.href = 'https://www.youtube.com';
+         // Popup-Inhalt und Styling definieren
+    var popupContent = "<h1>Du bist nicht alt genug!</h1><p>Dies ist ein Popup-Fenster.</p>";
+    var popupStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: black; padding: 20px;";
+
+    // Popup-Element erstellen und Inhalt einfügen
+    var popup = document.createElement("div");
+    popup.innerHTML = popupContent;
+    popup.style = popupStyle;
+
+    // Popup dem body-Element hinzufügen
+    document.body.appendChild(popup);
     }
 
 });
---
+++ index2.js
@@ -0,0 +1,26 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', function () {
+
+// Popup-Inhalt und Styling definieren
+var popupContent = "<h1>Altersüberprüfung</h1><p>Um diese Seite zu besuchen, musst du älter als 18 Jahre alt sein.</p><button id='older'>Ja, ich bin 18</button><button id='younger'>Nein, ich bin jünger</button>";
+var popupStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: black; padding: 20px;";
+
+// Popup-Element erstellen und Inhalt einfügen
+var popup = document.createElement("div");
+popup.innerHTML = popupContent;
+popup.style = popupStyle;
+
+// Popup dem body-Element hinzufügen
+document.body.appendChild(popup);
+
+// Popup schließen
+document.getElementById("older").addEventListener("click", function () {
+    document.body.removeChild(popup);
+});
+
+// Anderer Knopf wird gedrückt 
+document.getElementById('younger').addEventListener('click',()=>{
+    window.location.href = 'alter.html';
+});
+});
