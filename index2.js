0912fbf75de275e61473071a94ad4c5e210a254c
FS <francesco9721@gmail.com>
2023-05-20 23:40:38 +0200

    Abstand der Buttons vergrößert

--- index2.js
+++ index2.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 // Popup-Inhalt und Styling definieren
-var popupContent = "<h1>Altersüberprüfung</h1><p>Um diese Seite zu besuchen, musst du älter als 18 Jahre alt sein.</p><button id='older'>Ja, ich bin 18</button><button id='younger'>Nein, ich bin jünger</button>";
+var popupContent = "<h1>Altersüberprüfung</h1><p>Um diese Seite zu besuchen, musst du älter als 18 Jahre alt sein.</p><button id='older'>Ja, ich bin 18</button><button id='younger' style='margin-left: 50px'>Nein, ich bin jünger</button>";
 var popupStyle = "position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); background: black; padding: 700px; width: 100%; height: 250px; text-align: center;";
 
 // Popup-Element erstellen und Inhalt einfügen
