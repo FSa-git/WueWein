1543f785d4ecce5775a202667656e1ab95df24c0
FS <francesco9721@gmail.com>
2023-05-20 17:41:37 +0200

    Altersüberprüfung

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const progressBarEl = document.getElementById('progress-bar');
+// const progressBarEl = document.getElementById('progress-bar');
 
-window.addEventListener("scroll", ()=>{
+// window.addEventListener("scroll", ()=>{
 
-    let height = document.body.scrollHeight - window.innerHeight;
-    let scrollPosition = document.documentElement.scrollTop;
-    let width = (scrollPosition / height) * 100;
-    progressBarEl.style.width = '${width}%';
+//     let height = document.body.scrollHeight - window.innerHeight;
+//     let scrollPosition = document.documentElement.scrollTop;
+//     let width = (scrollPosition / height) * 100;
+//     progressBarEl.style.width = '${width}%';
 
-});
+// });
+
+function verifyAge() {
+    var birthDate = new Date(document.getElementById('birthdate').value);
+    var currentDate = new Date();
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    if (age >= 18) {
+        window.location.href = 'index.html';
+    } else {
+        window.location.href = 'https://www.youtube.com';
+    }
+}
